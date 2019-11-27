d5e209885680895cc2275b1551a2a0d685f346f7
MuYang <365312137@qq.com>
2019-11-27 08:53:44 +0800

    JWT-无状态登陆

--- leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
@@ -22,15 +22,16 @@ public class UserController {
 
     /**
      * 校验数据是否可用
+     *
      * @param data 要校验的数据
      * @param type 要校验的类型，1-用户名，2-手机
      * @return
      */
     @GetMapping("/check/{data}/{type}")
-    public ResponseEntity<Boolean> checkUser(@PathVariable("data")String data, @PathVariable("type")Integer type){
+    public ResponseEntity<Boolean> checkUser(@PathVariable("data") String data, @PathVariable("type") Integer type) {
         Boolean bool = this.userService.checkUser(data, type);
         //判断校验结果是否为null
-        if (bool == null){
+        if (bool == null) {
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(bool);
@@ -38,37 +39,40 @@ public class UserController {
 
     /**
      * 发送验证码
+     *
      * @param phone 手机
      * @return
      */
     @PostMapping("code")
-    public ResponseEntity<Void> sendVeifyCode(@RequestParam("phone") String phone){
+    public ResponseEntity<Void> sendVeifyCode(@RequestParam("phone") String phone) {
         this.userService.sendVeifyCode(phone);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     /**
      * 用户注册
+     *
      * @param user 用户数据
      * @param code 验证码
      * @return
      */
     @PostMapping("register")
-    public ResponseEntity<Void> userRegister(@Valid User user, @RequestParam("code")String code){
+    public ResponseEntity<Void> userRegister(@Valid User user, @RequestParam("code") String code) {
         this.userService.register(user, code);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     /**
      * 根据用户名和密码查询用户
+     *
      * @param username 用户名
      * @param password 密码
      * @return
      */
     @GetMapping("query")
-    public ResponseEntity<User> queryUser(@RequestParam("username")String username, @RequestParam("password")String password){
+    public ResponseEntity<User> queryUser(@RequestParam("username") String username, @RequestParam("password") String password) {
         User user = this.userService.queryUser(username, password);
-        if (user == null){
+        if (user == null) {
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(user);
